6092be85166d13d600f52abb965d2ba79d58691d
chandu <bigc@chandu.(none)>
2014-05-14 09:40:36 +0530

    timepass

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tim::Application.config.secret_key_base = '836736265617e0bc100b49a9696d627edec5738d54b456c68995ae0cc4934864b8e1ded20b325830e5d49c8d498d03c2ea2244763cdf4e7b2a5663cced7e5418'
